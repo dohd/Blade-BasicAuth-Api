71445f6d3f66c77e2efabb3319a9253ebb40d38a
Dohd <alloicebrian@gmail.com>
2021-04-13 11:36:26 +0300

    Add before_route context

--- src/main/java/com/hackermode/demo/App.java
+++ src/main/java/com/hackermode/demo/App.java
@@ -6,6 +6,8 @@ public class App
 {
     public static void main(String[] args) throws Exception {
         Blade blade = Blade.of();
+        blade.before("/auth/logout", ctx -> CookieService.verify(ctx));
+        blade.before("/auth/password-reset/:id", ctx -> CookieService.verify(ctx));
         blade.start(App.class, args);
     }
 }
